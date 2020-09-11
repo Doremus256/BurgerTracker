c6c5c454b078fb4f6f51e5399d3751962078bbf3
Brad Doremus <rbdoremus13@gmail.com>
2020-09-11 16:10:00 -0500

    added mysql connection and seeded table

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false)
+, ("Spicy Chicken Burger", false)
+, ("Veggie Burger", false)
+, ("BBQ Burger", false);
